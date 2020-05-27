f5d7990211afdf30a71797bb2f16f8c1baa7eb55
Nathan Gobinet <nathan.gobinet@gmail.com>
2020-05-27 14:15:26 +0200

    Modify the parameter button

--- components/ParameterButton.js
+++ components/ParameterButton.js
@@ -1,10 +1,23 @@
 import * as React from 'react';
-import { Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
+
+import Colors from '../constants/Colors'
 
 export default function ParameterButton({ navigation }) {
   return (
-    <Button 
-      onPress={() => navigation.navigate('Paramètres')} title="Paramètres" 
+    <Ionicons 
+      style= {styles.icon}
+      onPress={() => navigation.navigate('Paramètres')} 
+      name="md-settings" 
+      size={24} 
+      color={Colors.white} 
     />
   );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    marginRight: 25,
+  }
+});
