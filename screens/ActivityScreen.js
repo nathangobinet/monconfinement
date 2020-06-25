e21abc21300733a66388cc0ba4c5352e1199781a
Cyril Gourgouillon <cyrilgour@gmail.com>
2020-06-25 11:12:13 +0200

    add title over text - #4

--- screens/ActivityScreen.js
+++ screens/ActivityScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text,  View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 import Colors from '../constants/Colors'
 import ParameterButton from '../components/ParameterButton';
@@ -18,15 +18,51 @@ export default function ActivityScreen({ route, navigation}) {
 
   return (
     <View style={styles.container}>
+      <View>
+				<Image
+          style={styles.activityImage}
+					source={img}
+				/>
+        <View style={styles.overTitleBackground}>
+          <Text style={styles.overTitle} >Défoulez vous, tout en étant en règle.</Text>
+        </View>
+			</View>
+      <Text>Yes</Text>
       <Text>{titre}{img}{desc}{type}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  
+  activityImage: {
+    backgroundColor: Colors.primary,
+    height: 170,
+    marginTop: -10,
+    width: '100%',
+  },
+  
   container: {
     backgroundColor: Colors.background,
     flex: 1,
     paddingTop: 10
   },
+
+  overTitle: {
+    backgroundColor: Colors.text,
+    color: Colors.white,
+    fontWeight: 'Bold',
+    opacity: 1,
+    position: 'absolute',
+  },
+
+  overTitleBackground: {
+    backgroundColor: Colors.text,
+    bottom: 50,
+    marginLeft: 25,
+    marginRight: 25,
+    padding: 15,
+    position: 'relative',
+  },
+
 });
